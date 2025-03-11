cbc05626980f0810cd99dd5948711358b1cae510
Dirk Eddelbuettel <edd@debian.org>
2025-03-11 14:23:34 -0500

    Minor polish in example and test file

--- tests/simpleTest.R
+++ tests/simpleTest.R
@@ -3,10 +3,12 @@ library(RcppNLoptExample)
 
 expected <- c(0.3333333, 0.2962963)
 
+verbose <- TRUE
+
 ## newer nlopt versions require lower tolerance too
-val <- testConstrainedProblem("MMA")
+val <- testConstrainedProblem("MMA", verbose)
 stopifnot(all.equal(val, expected, tolerance=1e-3))
 
 ## expected slight difference in parameters hence tolerance set lower
-val <- testConstrainedProblem("COBYLA")
+val <- testConstrainedProblem("COBYLA", verbose)
 stopifnot(all.equal(val, expected, tolerance=1e-3))
