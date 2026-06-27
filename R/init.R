2fcbcf61414249327fea4390276a632cac89a521
Dirk Eddelbuettel <edd@debian.org>
2026-06-27 07:29:58 -0500

    Add package load-time log level setter with fallback default

---
+++ R/init.R
@@ -0,0 +1,9 @@
+
+.onLoad <- function(libname, pkgname) {
+    ## retrieve optional logging level, default to 'info'
+    lvl <- Sys.getenv("TL_LEVEL", "info")
+    ## set level (or default of 'info')
+    tl::set_level(lvl)
+    ## state level set (if level is at least 'debug')
+    tl::debug("[.onLoad] Set tl level to {}", lvl)
+}
