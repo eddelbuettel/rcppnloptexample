83fd34d5583de091de90414541f0faf8327e438b
Dirk Eddelbuettel <edd@debian.org>
2026-06-28 06:43:35 -0500

    Use tl::init() where available

--- R/init.R
+++ R/init.R
@@ -1,9 +1,13 @@
 
 .onLoad <- function(libname, pkgname) {
-    ## retrieve optional logging level, default to 'info'
-    lvl <- Sys.getenv("TL_LEVEL", "info")
-    ## set level (or default of 'info')
-    tl::set_level(lvl)
-    ## state level set (if level is at least 'debug')
-    tl::debug("[.onLoad] Set tl level to {}", lvl)
+    if (packageVersion("tl") > "0.0.1") {
+        tl::init() # set log level from envirornment variable, if given, or default
+    } else {
+        ## retrieve optional logging level, default to 'info'
+        lvl <- Sys.getenv("TL_LEVEL", "info")
+        ## set level (or default of 'info')
+        tl::set_level(lvl)
+        ## state level set (if level is at least 'debug')
+        tl::debug("[.onLoad] Set tl level to {}", lvl)
+    }
 }
